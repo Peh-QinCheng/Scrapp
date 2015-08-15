f4a4c8b15cdc31e68339ccb1651f57925a260292
Peh-QinCheng <pqc92@hotmail.com>
2015-08-16 01:26:05 +0800

    schema changed from migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815072123) do
+ActiveRecord::Schema.define(version: 20150815172035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,10 @@ ActiveRecord::Schema.define(version: 20150815072123) do
     t.string   "name"
     t.text     "description"
     t.string   "picture"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "email"
+    t.boolean  "deal",        default: false
   end
 
 end
