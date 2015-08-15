81510478cbb95a6b73b4d1fbb6156eccff1fd147
Peh-QinCheng <pqc92@hotmail.com>
2015-08-15 15:53:10 +0800

    changes to item model and view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814050404) do
+ActiveRecord::Schema.define(version: 20150815072123) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.float    "price"
@@ -20,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150814050404) do
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "email"
   end
 
 end
