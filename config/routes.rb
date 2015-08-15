ecabb5f3b51c3405d2995a0ba8d0697abb9c34f7
Peh-QinCheng <pqc92@hotmail.com>
2015-08-16 01:25:44 +0800

    added close_deal patch route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'items#index'
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create] do 
+    patch :close_deal, on: :member
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
